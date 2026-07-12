d71508ac115707b4e130534d57320e822f455878
Sai Sreeja Gummi <62544065+sreejareddy345@users.noreply.github.com>
2026-07-12 20:58:48 +0530

    Delete force-app/main/default/lwc/parentChild directory

--- force-app/main/default/lwc/parentChild/parentChild.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class ParentChild extends LightningElement {
-    parentMessage="Hi, message from parent"  
-    messageChild;
-    handleChild(evnt){
-        this.messageChild= evnt.detail;
-    }
-}
